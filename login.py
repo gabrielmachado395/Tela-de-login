70ed737d62a16194735fb6afdd3374295e97bd21
Gabriel Machado <gabrielryuzaki395@gmail.com>
2021-09-08 20:54:10 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,15 @@
+from time import sleep
+
 usuario = 1234
 senha = 9999
 erros = 0
+erros_bloqueio = 0
 
-usuario_acesso = int(input('Digite o código de usuário: '))
+print('-=' * 11)
+print('{:^22}' .format('OLÁ USUÁRIO'))
+print('-=' * 11)
+
+usuario_acesso = int(input('Digite o seu código de acesso: '))
 
 if usuario_acesso != usuario:
     print('Usuário invalido.')
@@ -12,10 +19,13 @@ elif usuario_acesso == usuario:
     senha_acesso = int(input('Digite sua senha: '))
 
     while senha_acesso != senha:
+        print('-=' * 11)
         opcoes = int(input('''[1] Tentar novamente\n[0] Encerrar sistema\nDigite uma das opções: '''))
+        print('-=' * 11)
 
         if opcoes == 1:
             senha_acesso = int(input('Digite sua senha novamente: '))
+            erros += 1
 
         elif opcoes != 1 and opcoes != 0:
             print('Opção invalida. Tente novamente.')
@@ -24,6 +34,22 @@ elif usuario_acesso == usuario:
             print('Sistema encerrado.')
             break
 
+        if erros == 5:
+            print('-=' * 11)
+            print('Você errou sua senha várias vezes, espere 5 segundos antes de tentar novamente.')
+            erros = 0
+            erros_bloqueio += 1
+
+            for espera in range(1, 6):
+                print(espera)
+                sleep(1)
+
+        elif erros_bloqueio == 3:
+            print('Você excedeu o limite de tentativas.\nO sistema foi bloqueado.')
+            break
+
     if senha_acesso == senha:
+        print('-=' * 11)
         print('Acesso permitido.')
         print('Bem-Vindo(a) Usuário.')
+        print('-=' * 11)
